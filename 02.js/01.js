c36782b8ae50566b11a76ac306b62fbcacd6f94f
JhonneyQ <kananqadirov2005@gmail.com>
2024-10-30 10:44:03 +0400

    commit

---
+++ 02.js/01.js
@@ -0,0 +1,104 @@
+// let a = +prompt("reqem1");
+// let b = +prompt("reqem2");
+// let c = +prompt("reqem3");
+
+// if(a+b>c+a && a+b>c+b){
+//     console.log(a + b);
+// }else if(c+a>a+b && c+a>b+c){
+//     console.log(c+a);
+// }else if(b+c>c+a && b+c>a+b){
+//     console.log(b+c);
+// }
+
+// let a = prompt("reqem1");
+// let b = prompt("reqem2");
+// let c = prompt("reqem3");
+
+// if(a+b+c>b+a+c && a+b+c>c+b+a && a+b+c>c+a+b){
+//     console.log(a + b + c);
+//     console.log(c+b+a);
+// }else if(c +a + b>a+b+c && c+a+b>b+c+a && c+a+b>c+b+a){
+//     console.log(c+a+b);
+//     console.log(b+a+c);
+// }else if(b+c+a>c+a+b && b+c+a>a+b+c && b+c+a>c+b+a){
+//     console.log(b+c+a);
+//     console.log(a+c+b);
+// }else if(c+b+a>a+b+c && c+a+b>b+c+a && c+b+a>c+a+b){
+//     console.log(c+b+a);
+//     console.log(a+b+c);
+// }
+
+// let a = +prompt("katet1");
+// let b = +prompt("katet2");
+// let c = +prompt("katet3");
+
+// // if(a*a+b*b == c)
+// if(a+b>c){
+//     console.log("duzbucaqli ucbucagdir");
+// }else{
+//     console.log("deyil")
+// }
+
+
+// let a = prompt("3 reqemli eded yazin");
+// console.log(`first ${a[0]} second ${a[1]} third ${a[2]}`);
+
+// a = +prompt("ay reqmi");
+// b = 31;
+// c = 30;
+// d = 28;
+
+// switch(a){
+//     case(1):
+//         console.log("yanvar", b, "gun");
+//         break;
+    
+//     case(2):
+//         console.log("fevral", d, "gun");
+//         break;
+
+//     case(3):
+//         console.log("mart", b, "gun");
+//         break;
+
+//     case(4):
+//         console.log("aprel", c, "gun");
+//         break;
+
+//     case(5):
+//         console.log("may", b, "gun")
+//         break
+//     case(6):
+//         console.log("iyun", c, "gun")
+//         break
+
+//     case(7):
+//         console.log("iyul", b, "gun")
+//         break
+
+//     case(8):
+//         console.log("avqust", c, "gun")
+//         break
+
+//     case(9):
+//         console.log("sentyabir", b, "gun")
+//         break
+
+//     case(10):
+//         console.log("oktyabir", c, "gun")
+//         break
+
+//     case(11):
+//         console.log("noyabir", b, "gun")
+//         break
+
+//     case(12):
+//         console.log("dakabir", c, "gun")
+//         break
+//     default:
+//         console.log("olmaz  :3");
+//         break;
+    
+    
+// }
+
