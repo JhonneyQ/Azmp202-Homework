0678e9ed474cdc8ef9f441dcadc32b551ad21af3
JhonneyQ <kananqadirov2005@gmail.com>
2024-10-31 12:18:01 +0400

    commit

---
+++ 01-js-variables.control/01.js
@@ -0,0 +1,89 @@
+// let a = +prompt("eded")
+
+// let b = a.toString()
+
+// let c = +b[0]
+// let d = +b[1]
+// let e = +b[2]
+// let f = c+d+e
+
+// console.log("f ", b[0] , "s", b[1], "t", b[2]);
+
+// console.log(c+d+e)
+// console.log(c*d*e)
+// console.log(f/3)
+// let b = 0
+
+// for (let index = 1; index < a; index++) {
+//     if(a%index==0){
+//         console.log(index);
+//         b++
+//     }
+    
+// }
+// console.log(b)
+// let a = 0
+// let c = 0
+// let b
+// for (let index = 0; index <= 10; index++) {
+//     b = index *index
+//     console.log(a++,"x",c++,"=",b)
+    
+// }
+
+// let a= 0
+// for (let index = 0; index < 10; index++) {
+//     b = a++
+//     c = b*b
+//     d = b*c
+//     console.log(b,c,d)
+// }
+
+
+// sum = 0
+// sum2 = 0
+// for (let index = 0; index < 100; index++) {
+//     if(index%2==0){
+//         sum+=index
+
+//     }
+//     if (index%2==1) {
+//         sum2+=index
+//     }
+    
+// }
+// console.log(sum)
+// console.log(sum2)
+
+
+// let a = [4,2,67,7,9,3,12,46,32,19]
+// let b = a[a.length-1]
+// let c = a[0]
+// for (let index = 1; index < a.length; index++) {
+//     if(c>a[index]){
+       
+//     }else{
+//         c=a[index]
+//     }
+//     if(b<a[index]){
+        
+//     }else{
+//         b=a[index]
+//     }
+    
+// }
+// console.log(c)
+// console.log(b)
+
+
+// let sampleNews = `As software developers, the natural tendency is to start developing applications based on your ownhands-on experience and knowledge right away. However, overtime issues in the application arise, adaptations to changes,
+// and new features happen.`;
+// let count = 0
+
+// for (let index = 0; index < sampleNews.length; index++) {
+//     if(sampleNews[index]===" "){
+//         count++
+//     }
+    
+// }
+// console.log(count)
