08662d5cdcfc5aca93c42d07585f8612ea9b0d65
JhonneyQ <kananqadirov2005@gmail.com>
2024-11-05 02:11:09 +0400

    commit

---
+++ 03-string-and-arrays/01.js
@@ -0,0 +1,81 @@
+// let a = "Salam"
+// a =a.replaceAll("a","o")
+// console.log(a)
+// let a =  ["milk", "coffe", "tea", "honey"]
+// a.pop()
+// a.unshift("meat")
+// a.push("sugar")
+// a = a.join(" ").replace("tea","grean-tea").split(" ")
+// console.log(a)
+
+// const countries = [
+//     'Albania',
+//     'Bolivia',
+//     'Canada',
+//     'Denmark',
+//     'Ethiopia',
+//     'Finland',
+//     'Germany',
+//     'Hungary',
+//     'Ireland',
+//     'Japan',
+//     'Kenya'
+//   ]
+
+//   countries.reverse()
+//   console.log(countries)
+//   countries.splice(2,2,"azerbaijan")
+//   console.log(countries)
+
+
+// let frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+// let backEnd = [' Node','Express', 'MongoDB']
+// let fullstack
+
+// frontEnd = frontEnd.join(" ")
+// backEnd = backEnd.join(" ")
+// fullstack = rfontEnd+backEnd
+// fullstack = fullstack.split(" ")
+
+// console.log(fullstack)
+
+let itCompanies = ["Facebook", "Google","Microsoft","Apple", "IBM", "Oracle", "Amazon"]
+// console.log(itCompanies.slice(3))
+// console.log(itCompanies.slice(0, 4))
+// console.log(itCompanies.slice(2, 6))
+// console.log(itCompanies.sort())
+// itCompanies = itCompanies.join(" ")
+// console.log(itCompanies.toUpperCase().split(" "))
+// if(itCompanies.includes("Google")){
+//     console.log("var")
+// }else{
+//     console.log("yoxdu")
+// }
+
+
+
+
+// for (let index = 0; index < itCompanies.length; index++) {
+//     let count = 0;
+//     // Use itCompanies[index] to get the company name
+//     for (let index2 = 0; index2 < itCompanies[index].length; index2++) {
+//         if (itCompanies[index][index2] === "o") {
+//             count++;
+//         }
+//     }
+//     console.log({count});
+//     if(count>1){
+//         console.log(itCompanies[index],true)
+//     }else{
+//         console.log(itCompanies[index],false)
+//     }
+// }
+
+// function ful(a,b,c){
+//     a =a.join(c)
+//     b = b.join(c)
+//     let d = (a+b)
+//     return d
+// }
+
+// console.log(ful( [1,2],[3,4],"*"))
