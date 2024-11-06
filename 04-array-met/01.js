75f670885d7369b064906530d958050a8eedfdd0
JhonneyQ <kananqadirov2005@gmail.com>
2024-11-06 04:16:26 +0400

    commit

---
+++ 04-array-met/01.js
@@ -0,0 +1,134 @@
+let arr = [
+    {
+      name: "test",
+      key: 1,
+    },
+    {
+      name: "task",
+      key: 2,
+    },
+    {
+      name: "tanqo",
+      key: 3,
+    },
+    {
+      name: "like",
+      key: 4,
+    },
+    {
+      name: "task",
+      key: 5,
+    },
+    {
+      name: "trust",
+      key: 6,
+    },
+    {
+      name: "test",
+      key: 7,
+    },
+    {
+      name: "last",
+      key: 8,
+    },
+    {
+      name: "tanqo",
+      key: 9,
+    },
+    {
+      name: "elephant",
+      key: 10,
+    },
+    {
+      name: "love",
+      key: 11,
+    },
+    {
+      name: "small",
+      key: 12,
+    },
+    {
+      name: "little",
+      key: 13,
+    },
+];
+
+// tar = arr.filter((element)=>{
+//     if(element.name[0]=="t"){
+//         return element.name
+//     }
+// })
+
+// console.log(tar)
+
+// tar = arr.forEach((element)=>{
+//     if(element.name[0]=="t" && element.name[element.name.length-1]== "t"){
+//         console.log(element.name)
+//     }
+// })
+// let a = 0
+// tar = arr.filter((element)=>{
+//     if(element.name[0]=="t" && element.name[element.name.length-1]== "t"){
+//         for (let index = 0; index < arr.key; index++) {
+//             a+=arr.key
+            
+//         }
+//     }
+   
+// })
+
+// console.log(tar)
+
+
+// tar = arr.filter((element)=>{
+//     if(element.name[element.name.length-1]== "e"){
+//         element.name="SuperDev"
+//     }
+//     return element.name
+// })
+
+// console.log(tar)
+// a = ""
+// tar = arr.filter((element)=>{
+//     if(element.name.length>a.length){
+//         a= element.name
+//     }
+//     return a
+// })
+
+// console.log(a)
+
+
+
+// a = ""
+// b= 0
+//  arr.filter((element)=>{
+//     if(element.name.length>a.length){
+//         b= element.key
+//     }
+//    return b 
+// })
+// console.log(b)
+
+// a = ""
+// b = 0
+//  arr.filter((element,index)=>{
+//     if(element.name.length>a.length){
+//         b =index.name
+//     }
+//     return b
+    
+// })
+
+// console.log(b)
+
+
+// b= []
+// tar = arr.filter((element)=>{
+//     if(element.name.length==4){
+//         b.push(element.name)
+//     }
+//     return b
+// })
+
+// console.log(b)
