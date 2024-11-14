e36d058171570c531f26066eee89ad8978fd642f
JhonneyQ <kananqadirov2005@gmail.com>
2024-11-14 12:54:45 +0400

    commit

--- 05-dom/03.js
+++ 05-dom/03.js
@@ -1,31 +1,38 @@
-const a = document.querySelector(".reqem1"); 
-const b = document.querySelector(".reqem2"); 
+const a = document.querySelector(".req1"); 
+const b = document.querySelector(".req2"); 
 const plus = document.querySelector(".plus");  
 const minus = document.querySelector(".minus");  
 const divide = document.querySelector(".divide"); 
 const multiply = document.querySelector(".multiply"); 
 const result = document.querySelector(".result");  
-const a1 = parseFloat(a.value)
-const b1 = parseFloat(b.value)
-let c = 0;
+
+
 
 
 plus.addEventListener("click", () => {
-    c = a1+b1;
+    const a1 = +a.value;
+    const b1 = +b.value;
+    const c = a1 + b1;
     result.textContent = c;  
 });
 
 minus.addEventListener("click", () => {
-    c = a1 - b1;
+    const a1 = +a.value;
+    const b1 = +b.value;
+    const c = a1 - b1;
     result.textContent = c;  
 });
 
 divide.addEventListener("click", () => {
-    c = a1 / b1;
+    const a1 = +a.value;
+    const b1 = +b.value;
+    const c = a1 / b1;
     result.textContent = c; 
 });
 
 multiply.addEventListener("click", () => {
-    c = a1*b1;
+    const a1 = +a.value;
+    const b1 = +b.value;
+    const c = a1 * b1;
     result.textContent = c;  
 });
