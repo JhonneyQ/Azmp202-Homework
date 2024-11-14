0890f68706952c3de7045ec20d8287c81df7d854
JhonneyQ <kananqadirov2005@gmail.com>
2024-11-14 12:25:44 +0400

    commit

---
+++ 05-dom/01.js
@@ -0,0 +1,19 @@
+// const inBut = document.querySelector(".in");
+// const text = document.querySelector(".text")
+// const deBut = document.querySelector('.de');
+// const sty = document.querySelector(".sty")
+// const inp = document.querySelector(".inp")
+// let fontSize = 16
+// inBut.addEventListener('click', () => {
+//     fontSize += 2;
+//     text.style.fontSize = fontSize + 'px';
+//   });
+
+//   deBut.addEventListener('click', () => {
+//     fontSize -= 2;
+//     text.style.fontSize = fontSize + 'px';
+//   });
+//   sty.addEventListener('click', () => {
+//     const color = inp.value
+//     text.style.color = color
+//   });
---
+++ 05-dom/02.js
@@ -0,0 +1,42 @@
+// const boxes = document.createElement("div");
+// boxes.style.width = "1200px";  
+// boxes.style.display = "flex";  
+// boxes.style.flexWrap = "wrap";  
+// boxes.style.justifyContent = "center";  
+// boxes.style.gap = "10px";  
+
+// const box = document.createElement("div");
+// box.setAttribute("class", "box");
+// box.style.width = "960px";
+// box.style.height = "360px";
+// box.style.backgroundColor = "red";
+
+
+// const box2 = document.createElement("div");
+// box2.style.width = "290px";
+// box2.style.height = "180px";
+// box2.style.backgroundColor = "blue";
+// box2.setAttribute("class", "abc");
+
+
+// const box3 = document.createElement("div");
+// box3.style.width = "290px";
+// box3.style.height = "180px";
+// box3.style.backgroundColor = "blue";
+// box3.setAttribute("class", "abc");
+
+
+// const box4 = document.createElement("div");
+// box4.style.width = "290px";
+// box4.style.height = "180px";
+// box4.style.backgroundColor = "blue";
+// box4.setAttribute("class", "abc");
+
+
+// boxes.append(box);
+// boxes.append(box2);
+// boxes.append(box3);
+// boxes.append(box4);
+
+
+// document.body.append(boxes);
