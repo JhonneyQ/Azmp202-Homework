9a7230683cad5a4efb087407a08113516d02cebe
JhonneyQ <kananqadirov2005@gmail.com>
2024-11-19 01:22:47 +0400

    commit

--- to-do/01.js
+++ to-do/01.js
@@ -1,75 +1,92 @@
-const input = document.querySelector(".input");
-const add = document.querySelector(".add");
-const todoList = document.querySelector(".todo-list");
-const filter = document.querySelector(".filter")
+    const input = document.querySelector(".input");
+    const add = document.querySelector(".add");
+    const todoList = document.querySelector(".todo-list");
+    const filter = document.querySelector(".filter")
+    const clear = document.querySelector(".clear")
 
-add.disabled = true;
-
-input.addEventListener("input", () => {
-    if (input.value.trim() === "") {
-        add.disabled = true;
-    } else {
-        add.disabled = false;
-    }
-});
-
-add.addEventListener("click", () => {
-    const div = document.createElement("div");
-    const par = document.createElement("p");
-    const radio = document.createElement("input")
-    const butdel = document.createElement("button");
-    const change = document.createElement("button");
-
-
-    div.style.display = "flex";
-    div.style.gap = "15px";
-    div.classList.add("task");
-
-    radio.setAttribute("type", "checkbox")
-    radio.setAttribute("name", "task");
-
-
-    par.textContent = input.value;
-    butdel.textContent = "Delete";
-    change.textContent = "change";
-
-  
-    div.appendChild(par);
-    div.appendChild(radio);
-    div.appendChild(butdel);
-    div.appendChild(change)
-    
-
-    todoList.appendChild(div);
-
-    butdel.addEventListener("click", () => {
-        todoList.removeChild(div);
-    });
-
-    change.addEventListener("click", ()=>{
-        const npar = prompt("new paragraph")
-        par.textContent = npar
-    })
-
-
-    input.value = "";
     add.disabled = true;
-});
 
-let filterActive = false;
-
-filter.addEventListener("click", () => {
-    filterActive = !filterActive; 
-    const tasks = document.querySelectorAll(".task");
-
-    tasks.forEach((task) => {
-        const checkbox = task.querySelector("input[type='checkbox']");
-        if (filterActive) {
-            task.style.display = checkbox.checked ? "flex" : "none";
+    input.addEventListener("input", () => {
+        if (input.value.trim() === "") {
+            add.disabled = true;
         } else {
-            task.style.display = "flex";
+            add.disabled = false;
         }
     });
 
-    filter.textContent = filterActive ? "Show All" : "Filter Checked";
-});
+    add.addEventListener("click", () => {
+        const div = document.createElement("div");
+        const par = document.createElement("p");
+        const radio = document.createElement("input")
+        const butdel = document.createElement("button");
+        const change = document.createElement("button");
+
+
+        div.style.display = "flex";
+        div.style.gap = "15px";
+        div.classList.add("task");
+
+        radio.setAttribute("type", "checkbox")
+        radio.setAttribute("name", "task");
+
+
+        par.textContent = input.value;
+        butdel.textContent = "Delete";
+        change.textContent = "change";
+        
+
+        if(par){
+            const taskr = JSON.parse(localStorage.getItem("task") || "[]");
+
+            taskr.push(par.textContent)
+            localStorage.setItem("task" , JSON.stringify(taskr))
+
+        }
+
+        clear.addEventListener("click", ()=>{
+            localStorage.clear()
+            todoList.removeChild(div)
+        })
+    
+        div.appendChild(par);
+        div.appendChild(radio);
+        div.appendChild(butdel);
+        div.appendChild(change)
+        
+
+        todoList.appendChild(div);
+
+        butdel.addEventListener("click", () => {
+            todoList.removeChild(div);
+            localStorage.removeItem(taskr)
+        });
+
+
+        change.addEventListener("click", ()=>{
+            const npar = prompt("new paragraph")
+            par.textContent = npar
+        })
+
+
+        input.value = "";
+        add.disabled = true;
+    });
+
+    let filterActive = false;
+
+    filter.addEventListener("click", () => {
+        filterActive = !filterActive; 
+        const tasks = document.querySelectorAll(".task");
+
+        tasks.forEach((task) => {
+            const checkbox = task.querySelector("input[type='checkbox']");
+            if (filterActive) {
+                task.style.display = checkbox.checked ? "flex" : "none";
+            } else {
+                task.style.display = "flex";
+            }
+        });
+
+        filter.textContent = filterActive ? "Show All" : "Filter Checked";
+    });
+
